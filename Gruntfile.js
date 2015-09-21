a11588e86645f8429173a16676755ed0b00f1032
Cyrille Chopelet <cyrille.chopelet@mines-nancy.org>
2015-09-21 19:53:48 +0000

    Configure correctly dependency to Grunt plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,6 +68,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-jsdoc');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-coveralls');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint', 'uglify', 'qunit', 'jsdoc']);
