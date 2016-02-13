1e08f7dd93b9eb51a527e38a1cc99d55658f1949
Cyrille Chopelet <cyrille.chopelet@mines-nancy.org>
2016-02-13 02:43:44 +0000

    Revert "Try augmenting timeout"
    This reverts commit e754e2572fe4b90ce8166003fe528341659be9c9.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,8 +25,6 @@ module.exports = function(grunt) {
 
     qunit: {
   		options: {
-  		  timeout: 30000,
-  		  "--web-security": "no",
   			coverage: {
   				src: [ "src/*.js" ],
   				instrumentedFiles: "temp/",
